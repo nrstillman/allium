6e1c77aacdd60ba4bcd53bd98e88f6379a4676a5
nrstillman <namid.stillman@gmail.com>
2022-02-07 13:26:16 +0000

    working animation generation

--- tmp.py
+++ tmp.py
@@ -1,5 +1,5 @@
 import allium 
-
+import random
 import glob
 import numpy as np
 import pandas as pd 
@@ -9,8 +9,16 @@ import matplotlib.animation as animation
 import networkx as nx
 from scipy.spatial import distance_matrix
 
+
+# TODO: 
+#    - Parameter sweep with different values
+#    - Seed sweep with single set of values
+
+sweep = True
 simulate = False
-if simulate:
+
+params = 
+if simulate or sweep:
     config = "include/config/simconfig_gnn.json"
     sim = allium.simulate.Sim(parameterFile = config, save_prob=1)
 
@@ -21,21 +29,30 @@ files.sort()
 
 # creating a blank window
 # for the animation
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(dpi=200)
 ax.clear()
 
 df = pd.read_csv(files[0])
+colors = [["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])][0] for n in range(len(df))]
 
 dist_matrix = pd.DataFrame(distance_matrix(df[['x','y']].values,df[['x','y']]), index=df.index, columns=df.index)
 
 interaction_range = 1
 adj_matrix = 1*(dist_matrix<interaction_range).values
-
 rows, cols = np.where(adj_matrix == 1)
 edges = zip(rows.tolist(), cols.tolist())
-gr = nx.Graph()
-gr.add_edges_from(edges)
-nx.draw(gr,[(x,y) for x,y in df[['x','y']].values], node_size=75)
+
+G = nx.Graph()
+G.add_edges_from(edges)
+
+pos = [(x,y) for x,y in df[['x','y']].values]
+node_colors = [colors[n] for n in G.nodes]
+labels = df.flag
+
+nx.draw(G,pos, node_size=df.radius*100,
+            node_color=node_colors, with_labels=True)
+
+# nx.draw_networkx_labels(G, pos, labels, font_size=22, font_color="whitesmoke")
 
 # animation function
 def animate(i):
@@ -45,14 +62,21 @@ def animate(i):
     
     dist_matrix = pd.DataFrame(distance_matrix(df[['x','y']].values,df[['x','y']]), index=df.index, columns=df.index)
     
-    interaction_range = 1.3
+    interaction_range = 1.2
     adj_matrix = 1*(dist_matrix<interaction_range).values
-
     rows, cols = np.where(adj_matrix == 1)
     edges = zip(rows.tolist(), cols.tolist())
-    gr = nx.Graph()
-    gr.add_edges_from(edges)
-    nx.draw(gr,[(x,y) for x,y in df[['x','y']].values], node_size=50)
+
+    G = nx.Graph()
+    G.add_edges_from(edges)
+
+    pos = [(x,y) for x,y in df[['x','y']].values]
+    node_colors = [colors[n] for n in G.nodes]
+    labels = df.flag
+
+    nx.draw(G, pos, node_size=df.radius*100,
+                node_color=node_colors, with_labels=True)
+    # nx.draw_networkx_labels(G, pos, labels, font_size=22, font_color="whitesmoke")
 
  
 # calling the animation function    
